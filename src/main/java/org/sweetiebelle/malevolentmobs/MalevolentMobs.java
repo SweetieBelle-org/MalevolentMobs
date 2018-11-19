155647d65e0f1b64a4bc18039c2c512825e45148
Sydney <sweetiebelle@sweetiebelle.org>
2018-11-19 18:50:19 -0500

    Initial commit

---
+++ src/main/java/org/sweetiebelle/malevolentmobs/MalevolentMobs.java
@@ -0,0 +1,31 @@
+package org.sweetiebelle.malevolentmobs;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = MalevolentMobs.MODID, name = MalevolentMobs.NAME, version = MalevolentMobs.VERSION)
+public class MalevolentMobs
+{
+    public static final String MODID = "malevolentmobs";
+    public static final String NAME = "Malevolent Mobs";
+    public static final String VERSION = "1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+
+    }
+}
